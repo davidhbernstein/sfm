ae33d31af36bca5435c4732743b4aa9c96f7cf5b
David Bernstein <davebernie89@yahoo.com>
2026-01-26 20:20:47 -0500

    sfa update
    The 'davidhbernstein/sfm' Github package has moved to CRAN with the new name 'sfa'.
    The 'sfa' CRAN package is now imported from 'davidhbernstein/sfm'.

--- R/start.R
+++ R/start.R
@@ -1,6 +1,4 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("The 'davidhbernstein/sfm' Github package has moved to CRAN with the new name 'sfa'.  
-  To install 'sfa' paste:
-  install.packages('sfa')
-  library(sfa) ")
+packageStartupMessage("The 'davidhbernstein/sfm' Github package has moved to CRAN with the new name 'sfa'.  
+The 'sfa' CRAN package is now imported from 'davidhbernstein/sfm'.")
 }
